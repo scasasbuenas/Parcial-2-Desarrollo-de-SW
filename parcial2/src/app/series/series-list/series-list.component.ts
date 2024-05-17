576cc27b6590d6065566645b2a1ab4f6ce6caf0e
scasasbuenas <s.casasbuenas@uniandes.edu.co>
2024-05-17 10:49:36 -0500

    jajajaj

--- parcial2/src/app/series/series-list/series-list.component.ts
+++ parcial2/src/app/series/series-list/series-list.component.ts
@@ -32,6 +32,31 @@ export class SeriesListComponent implements OnInit {
     console.log(series);
   }
 
+  getPromedioEpisodios(): number {
+    let totalEpisodios = 0;
+
+    for (const serie of this.series) {
+      totalEpisodios += serie.episode;
+    }
+
+    const cantidadSeries = this.series.length;
+    this.cantTotalEp = totalEpisodios / cantidadSeries;
+
+    return parseFloat(this.cantTotalEp.toFixed(2));
+  }
+
+  getPromedioRatings(): number {
+    let totalRatings = 0;
+
+    for (const serie of this.series) {
+      totalRatings += parseFloat(serie.Rating);
+    }
+
+    const cantidadSeries = this.series.length;
+    this.calificacionPromedio = totalRatings / cantidadSeries;
+
+    return parseFloat(this.calificacionPromedio.toFixed(2));
+  }
 
   ngOnInit() {
     this.getSeries();
